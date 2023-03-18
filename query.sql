687128be5f0472abe618d5586c690e7a0a2f375e
Kishor <117354246+Kishor-550@users.noreply.github.com>
2023-03-18 12:01:29 +0530

    Add files via upload

--- query.sql
+++ query.sql
@@ -10,51 +10,32 @@ SELECT employee_id, first_name,last_name,salary
 FROM exercise_hr.employees 
 ORDER BY salary ASC;
 
+-- 07-02-2023
 
-
-/*
-
-07-02-2023
-
-Note: USE exercise_hr;
-Write a query to calculate 171*214+625.
-Write a query to display the names (first_name, last_name) using alias name “First Name", "Last Name" from employees table.
-Write a query to get the names (first_name, last_name), salary, PF of all the employees (PF is calculated as 15% of salary).
-Write a query to get unique department ID from employee table.
-Self-study exercise (Use store):
-Return name, unit price, and a new price for all the products that is equal to 1.1 * unit_price.
-
-*/
-
+-- Write a query to calculate 171*214+625.
 
 SELECT 171*214 + 625;
 
 USE exercise_hr;
 
+-- Write a query to display the names (first_name, last_name) using alias name “First Name", "Last Name" from employees table.
 SELECT first_name AS "First Name",
 last_name AS "Last Name"
 FROM employees ;
 
-SELECT first_name, last_name, salary, salary*.15 AS 'PF' 
+-- Write a query to get the names (first_name, last_name), salary, PF of all the employees (PF is calculated as 15% of salary).
+SELECT first_name, last_name, salary, salary*.15 AS 'PF'
 FROM employees;
 
+-- Write a query to get unique department ID from employee table.
 SELECT DISTINCT DEPARTMENT_ID FROM employees;
 
 USE store;
 
+-- Return name, unit price, and a new price for all the products that is equal to 1.1 * unit_price.
 SELECT name,unit_price,(unit_price * 1.1) AS "new price" FROM order_items,order_statuses;
 
-/*
-
-08-02-2023
-
-Note : USE exercise_hr;
-Write a query to display the name (first_name, last_name) and hire date for all employees who were hired in 1987.
-Write a query to display the name (first_name, last_name) and salary for all employees whose salary is not in the range $10,000 through $15,000.
-Write a query to display the name (first_name, last_name) and department ID of all employees in departments 30 or 100 in ascending order.
-Write a query to display the name (first_name, last_name) and salary for all employees whose salary is not in the range $10,000 through $15,000 and are in department 30 or 100.
-Write a query to select all record from employees where last name in 'BLAKE', 'SCOTT', 'KING' and 'FORD'.
-*/
+-- 08-02-2023
 
 USE exercise_hr;
 
@@ -84,71 +65,52 @@ SELECT *
 FROM employees
 WHERE last_name IN ('BLAKE', 'SCOTT', 'KING','FORD');
 
-/*
-USE store;
- Return products with
--- quantity in stock equal to 49, 38, 72
--- no product with quantity_in_stock = 72.
-2. Return customers born
--- between 1/1/1990 and 1/1/2000
-3. Get the customers whose
--- 1. addresses contain TRAIL or AVENUE
--- 2. phone numbers end with 9
--- 3. phone numbers do not end with 9
-4. Get the customers whose
--- 1. first names are ELKA or AMBUR
--- 2. last names end with EY or ON
--- 3. last names start with MY or contains SE
--- 4. last names contains B followed by R or U
-*/
-
 USE store;
 
+-- Return products with quantity in stock equal to 49, 38, 72 and no product with quantity_in_stock = 72
 SELECT *
 FROM products
-WHERE quantity_in_stock IN (49,38,72);
+WHERE quantity_in_stock IN (49,38) AND quantity_in_stock <> 72;
 
+-- Return customers born between 1/1/1990 and 1/1/2000
 SELECT *
 FROM customers
 WHERE birth_date BETWEEN '1990-1-1' AND '2000-1-1';
 
+-- Get the customers whose addresses contain TRAIL or AVENUE
 SELECT *
 FROM customers
 WHERE address LIKE '%TRAIL%' OR address LIKE '%AVENUE%';
 
+-- Get the customers whose phone numbers end with 9
 SELECT *
 FROM customers
 WHERE phone LIKE '%9';
 
+-- Get the customers whose phone numbers do not end with 9
 SELECT *
 FROM customers
 WHERE phone NOT LIKE '%9';
 
+-- Get the customers whose first names are ELKA or AMBUR
 SELECT *
 FROM customers
-WHERE first_name LIKE 'ELKA' OR first_name LIKE 'AMBUR';
+WHERE first_name IN ('ELKA', 'AMBUR');
 
+-- Get the customers whose last names end with EY or ON
 SELECT *
 FROM customers
 WHERE last_name LIKE '%EY' OR last_name LIKE '%ON';
 
+-- Get the customers whose last names start with MY or contains SE
 SELECT *
 FROM customers
 WHERE last_name LIKE 'MY%' OR last_name LIKE '%SE%';
 
+-- Get the customers whose last names contains B followed by R or U
 SELECT *
 FROM customers
-WHERE last_name LIKE '%B%' AND (last_name LIKE '%R%' OR last_name LIKE '%U%');
-
-
-/*
-USE exercise_hr;
-Hint: LIKE '___' will find all 3 letter words.
-Write a query to display the first_name of all employees who have both "b" and "c" in their first name.
-Write a query to display the last name of employees whose names have exactly 6 characters.
-Write a query to display the last name of employees having 'e' as the third character.
-Write a query to display the jobs/designations available in the employees table.
-*/
+WHERE last_name LIKE '%B_R%' OR last_name LIKE '%B_U%';
 
 USE exercise_hr;
 
@@ -214,12 +176,6 @@ WHERE last_name REGEXP "[a-h]e";
 
 
 
-
-/* 
-
-*/
-
-
 SELECT *
 FROM customers
 WHERE last_name REGEXP "[gim]e";
@@ -283,19 +239,6 @@ EXPLAIN
 SELECT *
 FROM employees;
 
-/*
-Write a query to get the total salaries payable to employees.
-Write a query to get the maximum and minimum salary from employees table.
-Write a query to get the average salary and number of employees in the employees table.
-Write a query to get the number of employees working with the company.
-Write a query to get the number of distinct jobs available in the employees table.
-Write a query get all first name from employees table in upper case.
-Write a query to get the first 3 characters of first name from employees table.
-Write a query to get the names ('<first name> <last name>') (for example Ellen Abel, Sundar Ande etc.) as a single column of all the employees from employees table.
-Write a query to get the length of the employee names ('<first name> <last name>') from employees table.
-Write a query to get monthly salary (round 2 decimal places) of each and every employee
-Note : Assume the salary field provides the 'annual salary' information.
-*/
 
 USE exercise_hr;
 
@@ -354,22 +297,37 @@ FROM employees AS e INNER JOIN departments AS d
 ON e.department_id = d.department_id
 WHERE department_name = 'IT';
 
-/*
-Write a query to find the name (first_name, last name), department ID and name of all the employees.
-Write a query to find the name (first_name, last_name), job, department ID and name of the employees who works in London.
-Write a query to find the employee id, name (last_name) along with their manager_id and name (last_name).
-Write a query to find the name (first_name, last_name) and hire date of the employees who was hired after 'Jones'.
-Write a query to get the department name and number of employees in the department. (Requires GROUP BY)
-Write a query to find the employee ID, job title, number of days between ending date and starting date for all jobs in department 90.
-*/
-SELECT first_name,last_name,department_id,CONCAT(first_name, ' ',last_name) AS "Name"
+-- Find the name (first_name, last name), department ID and name of all the employees.
+SELECT first_name, last_name, department_id, CONCAT(first_name, ' ', last_name) AS "Name"
 FROM employees;
 
-SELECT e.first_name, e.last_name, e.job_id, e.department_id, d.department_name 
-FROM employees AS e INNER JOIN departments AS d ON (e.department_id = d.department_id) 
-INNER JOIN locations AS l ON (d.location_id = l.location_id) 
+--  Find the name (first_name, last_name), job, department ID and name of the employees who works in London.
+SELECT e.first_name, e.last_name, e.job_id, e.department_id, d.department_name
+FROM employees AS e
+INNER JOIN departments AS d ON (e.department_id = d.department_id)
+INNER JOIN locations AS l ON (d.location_id = l.location_id)
 WHERE LOWER(l.city) = 'London';
 
-SELECT e.employee_id, e.last_name, m.manager_id
-FROM employees AS e INNER JOIN departments AS m 
-ON e.manager_id = m.MANAGER_ID;
+-- Find the employee id, name (last_name) along with their manager_id and name (last_name).
+SELECT e.employee_id, e.last_name, m.manager_id, m.last_name AS "Manager Name"
+FROM employees AS e
+INNER JOIN employees AS m ON e.manager_id = m.employee_id;
+
+-- Find the name (first_name, last_name) and hire date of the employees who was hired after 'Jones'.
+SELECT e.first_name, e.last_name, e.hire_date
+FROM employees AS e
+JOIN employees AS self ON self.last_name = 'Jones'
+WHERE self.hire_date < e.hire_date;
+
+-- Get the department name and number of employees in the department.
+SELECT department_name AS "Department Name", COUNT(*) AS "No of Employees"
+FROM departments AS d
+INNER JOIN employees AS e ON e.department_id = d.department_id
+GROUP BY d.department_id, department_name
+ORDER BY department_name;
+
+-- Find the employee ID, job title, number of days between ending date and starting date for all jobs in department 90.
+SELECT employee_id, job_title, end_date-start_date AS "Days"
+FROM job_history
+NATURAL JOIN jobs
+WHERE department_id=90;
